1ef9b8ec263e12ec7773f806e18eff1255b44ac6
Dean Vinícius <deanvinicius2003@gmail.com>
2024-12-06 22:49:54 -0300

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="w-full h-screen flex justify-center items-center">
+    <div class="p-2 w-fit min-w-[80vh] min-h-[80vh] rounded-lg bg-neutral-800"></div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,ts,jsx,tsx,vue}"],
+  darkMode: 'media',
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: [
+          'Inter',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          "'Segoe UI'",
+          'Roboto',
+          'Oxygen',
+          'Ubuntu',
+          'Cantarell',
+          "'Fira Sans'",
+          "'Droid Sans'",
+          "'Helvetica Neue'",
+          'sans-serif'
+        ]
+      },
+      colors: {
+        // Adicione as cores personalizadas
+        'background-light': '#f0f0f0', // Cor de fundo no modo claro
+        'background-dark': '#1a1a1a',  // Cor de fundo no modo escuro
+      },
+      backgroundColor: {
+        // Defina as cores de fundo padrão
+        'background': '#f0f0f0', // Cor de fundo no modo claro
+      },
+    }
+  },
+  plugins: [],
+}
