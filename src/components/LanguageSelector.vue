5798bef90169f9fe38cc3a28eb1099d0dfdebdb5
DeanVini <dean.meneses@pgm.pma.rima>
2025-01-09 16:57:18 -0300

    feat: language selector (incomplete)

---
+++ src/components/LanguageSelector.vue
@@ -0,0 +1,12 @@
+<template></template>
+
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n'
+import LOCALES from '@/constants/locales'
+
+const { locale } = useI18n()
+
+const changeLanguage = (lang: string) => {
+  locale.value = lang
+}
+</script>
